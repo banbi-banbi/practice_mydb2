9a296b0be890c93a094dc57d03f15471d3a23793
banbi-banbi <takuto17oct@yahoo.co.jp>
2020-06-28 21:12:54 +0900

    復習作業：レッスン57済

--- memo2/index.php
+++ memo2/index.php
@@ -25,8 +25,23 @@ try {
       echo 'DB接続エラー：'.$e->getMessage();
 }
 
-$count = $db->exec('INSERT INTO my_items SET maker_id=1, item_name="もも", price=210, keyword="件詰,ピンク,甘い"');
-echo $count. '件のデータを挿入しました';
+// 【レッスン56】データ挿入
+// $count = $db->exec('INSERT INTO my_items SET maker_id=1, item_name="もも", price=210, keyword="件詰,ピンク,甘い"');
+// echo $count. '件のデータを挿入しました';
+
+// 【レッスン57】データ閲覧
+$records = $db->query('SELECT * FROM my_items');
+while($record = $records->fetch()) {
+  print($record['item_name']. "\n");
+
+  $recordの中の型とカラム名などの確認（独自）
+  while($record = $records->fetch()) {
+    var_dump($record);
+    echo('■■■■■■■■■■■■');
+  }
+  
+}
+
 ?>
 </pre>
 </main>
