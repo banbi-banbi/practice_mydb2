7177ef1894ae10a72b049ae5801612e7afc8382a
banbi-banbi <takuto17oct@yahoo.co.jp>
2020-06-29 11:07:27 +0900

    復習作業：59まで済

---
+++ memo2/input_do.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>    
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+  <?php
+  try {
+    $db = new PDO('mysql:dbname=mydb2;host=127.0.0.1;charset=utf8','root','');
+
+    // 危険な書き方（SQL文の中にPOSTを直接埋め込む）
+    // $db->exec('INSERT INTO memos SET memo="'. $_POST['memo']. '", created_at=NOW()');
+
+    // 安全な書き方（SQL文の中にPOSTを直接埋め込まない）
+    // $statement = $db->prepare('INSERT INTO memos SET memo=?, created_at=NOW()');
+    // $statement->execute(array($_POST['memo']));
+
+    // 安全な書き方（複数パラメーターの登録）
+    $statement = $db->prepare('INSERT INTO memos SET memo=?, created_at=NOW()');
+    $statement->bindParam(1, $_POST['memo']);
+    $statement->execute();
+
+    // 共通
+    echo 'メッセージが登録されました';
+
+  } catch (PDOException $e) {
+    echo 'DB接続エラー：'. $e->getMessage();
+  }
+  ?>
+</pre>
+</main>
+</body>    
+</html>
